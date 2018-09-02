c542e01f7a0e7ee50e06ecfc5d962e5b21a4246c
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-02 17:23:13 -0600

    adds spec test/functionality for goal indexw, needs styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+goal1 = Goal.create!(description: "Happy Hour", frequency: "once a month")
+group2 = Group.create!(title: "Work Friends")
+person1 = goal1.people.create!(name: "Sara", group_id: "#{group1.id}")
+person2 = goal1.people.create!(name: "George", group_id: "#{group1.id}")
+
+goal2 = Goal.create!(description: "Karaoke", frequency: "every other month")
+group2 = Group.create!(title: "College Friends")
+person3 = goal2.people.create!(name: "Laura", group_id: "#{group2.id}")
+person4 = goal2.people.create!(name: "Joe", group_id: "#{group2.id}")
--- spec/features/goals/user_can_see_all_goals_spec.rb
+++ spec/features/goals/user_can_see_all_goals_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 describe 'user can see all goals' do
   describe 'they visit /goals' do
-    
+    it 'displays goals with associated people' do
+      goal1 = Goal.create!(description: "Happy Hour", frequency: "once a month")
+      group = Group.create!(title: "Work Friends")
+      person1 = goal1.people.create!(name: "Sara", group_id: "#{group.id}")
+      person2 = goal1.people.create!(name: "George", group_id: "#{group.id}")
+
+      visit goals_path
+
+      expect(page).to have_content("Happy Hour")
+      expect(page).to have_content("Sara")
+      expect(page).to have_content("George")
+    end
+  end
+end
