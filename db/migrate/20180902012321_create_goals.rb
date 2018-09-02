3472e0b8e498c694d3f7af524bd23db4c1b808e9
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-02 16:52:03 -0600

    updates CreateGoal migration adding attribute frequency to replace hours, number of times, choice of unit

--- db/migrate/20180902012321_create_goals.rb
+++ db/migrate/20180902012321_create_goals.rb
@@ -2,9 +2,7 @@ class CreateGoals < ActiveRecord::Migration[5.1]
   def change
     create_table :goals do |t|
       t.string :description
-      t.float :hours
-      t.integer :number_of_times
-      t.string :choice_of_unit
+      t.string :frequency
 
       t.timestamps
     end
