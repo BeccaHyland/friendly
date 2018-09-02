aeae13856a20cb9c37253f2649ce8b861bccbbe8
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-02 16:03:43 -0600

    draft config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'dashboards/index' <-- why?
+  root 'dashboards#index'
+
+  resources :people, only: [:index]
+  resources :group do
+    resources :people
+  end
+
+  resources :goals
+  resources :person_goals do
+    resources :goalposts
+  end
+
 end
