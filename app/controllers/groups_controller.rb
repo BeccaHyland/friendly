532eac0d65d9a91ab64f5a40bc15072e4bb4bb9a
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-03 12:24:27 -0600

    Adds group create functionality

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,26 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
-  def edit
+  def new
+    @group = Group.new
+  end
 
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:success] = "#{@group.title} added!"
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title)
   end
 end
