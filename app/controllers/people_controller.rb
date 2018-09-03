bf9013d23838935334d3cb6b15a9d6691a0b0801
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-03 17:36:52 -0600

    adds new/create person functionality

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,6 +7,29 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
   end
 
+  def new
+    @person = Person.new
+    @groups = Group.all
+  end
+
+  def create
+    @person = Person.new(person_params)
+    @groups = Group.all
+    if @person.save
+      flash[:success]= "#{@person.name} added!"
+      redirect_to people_path
+    else
+      render :new
+    end
+  end
+
   def edit
+    @person = Person.find(params[:id])
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :birthday, :group_id, :important_date, :important_date_text)
   end
 end
