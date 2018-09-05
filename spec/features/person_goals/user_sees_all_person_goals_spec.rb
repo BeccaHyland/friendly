a48fd2e130fe0c3b0519dd7fa36b2217217eed3c
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-05 09:50:42 -0600

    move file, comment out pre-functional test

--- spec/features/person_goals/user_sees_all_person_goals_spec.rb
+++ spec/features/person_goals/user_sees_all_person_goals_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-describe 'user can see all person_goals'
-  describe 'user visits person show page'
-    it 'displays only person_goals associated with that person'
+describe 'user can see all person_goals' do
+  describe 'user visits person show page' do
+    it 'displays only person_goals associated with that person' do
+    end
+  end
+end
