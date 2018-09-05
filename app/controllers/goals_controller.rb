521a2482e35d9ae0a4dbb78facda3ff5ec411991
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-04 19:37:02 -0600

    adds goal edit functionality

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,4 +2,25 @@ class GoalsController < ApplicationController
   def index
     @goals = Goal.all
   end
+
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+    @goal.update(goal_params)
+    if @goal.save
+      flash[:success] = "#{@goal.description} Goal updated!"
+      redirect_to goals_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:description, :frequency)
+  end
 end
