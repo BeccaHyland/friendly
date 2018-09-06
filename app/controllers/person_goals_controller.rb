7e0916ae9b88890ca667ac10eb4cc49be3b25643
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-09-06 08:33:03 -0600

    bug person_goals_controller

--- app/controllers/person_goals_controller.rb
+++ app/controllers/person_goals_controller.rb
@@ -8,7 +8,7 @@ class PersonGoalsController < ApplicationController
     @person = Person.find(params[:person_id])
     @goal = Goal.find(params[:goal_id])
     @person_goal = PersonGoal.new(person_goal_params)
-    @person_goal[:title] = "#{@person.name)}: #{@goal.title}"
+    @person_goal[:title] = "#{@person.name}: #{@goal.title}"
     if @person_goal.save
       flash[:success] = "#{@person.name} added to #{@goal.title}!"
       redirect_to goals_path
